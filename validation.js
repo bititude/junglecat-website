5c755a989f205d88c217c75479cd9c653edba6b1
theertha <theertha@bititude.com>
2022-03-14 22:06:07 +0530

    validation added

---
+++ validation.js
@@ -0,0 +1,27 @@
+const name = document.form.name;
+const email= document.form.email;
+const message= document.form.message;
+function validated() {
+  if (name.value == '') {
+    name.nextElementSibling.style.display = 'block'
+    return false
+  } else {
+    name.nextElementSibling.style.display = 'none'
+  }
+
+  if (!email.value.match(
+/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+) ||email.value == '') {
+   email.nextElementSibling.style.display = 'block'
+    return false
+  } else {
+    email.nextElementSibling.style.display = 'none'
+  }
+
+  if (message.value == '') {
+   message.nextElementSibling.style.display = 'block'
+    return false
+  } else {
+    message.nextElementSibling.style.display = 'none'
+  }
+}
