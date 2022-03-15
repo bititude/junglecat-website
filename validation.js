e2c7ef728180e1c196a5321545574eea544bbbef
theertha <theertha@bititude.com>
2022-03-15 14:19:14 +0530

    carousel  changed

--- validation.js
+++ validation.js
@@ -1,6 +1,6 @@
-const name = document.form.name;
-const email= document.form.email;
-const message= document.form.message;
+const name = document.form.name
+const email = document.form.email
+const message = document.form.message
 function validated() {
   if (name.value == '') {
     name.nextElementSibling.style.display = 'block'
@@ -9,19 +9,32 @@ function validated() {
     name.nextElementSibling.style.display = 'none'
   }
 
-  if (!email.value.match(
-/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-) ||email.value == '') {
-   email.nextElementSibling.style.display = 'block'
+  if (
+    !email.value.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    ) ||
+    email.value == ''
+  ) {
+    email.nextElementSibling.style.display = 'block'
     return false
   } else {
     email.nextElementSibling.style.display = 'none'
   }
 
   if (message.value == '') {
-   message.nextElementSibling.style.display = 'block'
+    message.nextElementSibling.style.display = 'block'
     return false
   } else {
     message.nextElementSibling.style.display = 'none'
   }
+}
+function fun() {
+  document.getElementById('name').value = ''
+  document.getElementById('email').value = ''
+  document.getElementById('message').value = ''
+}
+function sendEmail(){
+  email.send({
+    
+  })
 }
