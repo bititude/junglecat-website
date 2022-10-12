93d407443105b54f7ddc7e1a47c7253e04438303
theertha <theertha@bititude.com>
2022-10-12 23:06:21 +0530

    image changes,scrolling

--- validation.js
+++ validation.js
@@ -1,79 +1,123 @@
-const name = document.form.name;
-const email = document.form.email;
-const message = document.form.message;
-let captchaResponseKey = "";
+const name = document.form.name
+const email = document.form.email
+const message = document.form.message
+let captchaResponseKey = ''
 
-const captchaError = document.getElementById("captcha-error");
-const myModalEl = document.querySelector(".mymodal");
-const modal = new window.bootstrap.Modal(myModalEl);
+const captchaError = document.getElementById('captcha-error')
+const myModalEl = document.querySelector('.mymodal')
+const modal = new window.bootstrap.Modal(myModalEl)
 
 function validated() {
-  if (name.value == "") {
-    name.nextElementSibling.style.display = "block";
-    return false;
+  if (name.value == '') {
+    name.nextElementSibling.style.display = 'block'
+    return false
   } else {
-    name.nextElementSibling.style.display = "none";
+    name.nextElementSibling.style.display = 'none'
   }
 
   if (
     !email.value.match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     ) ||
-    email.value == ""
+    email.value == ''
   ) {
-    email.nextElementSibling.style.display = "block";
-    return false;
+    email.nextElementSibling.style.display = 'block'
+    return false
   } else {
-    email.nextElementSibling.style.display = "none";
+    email.nextElementSibling.style.display = 'none'
   }
 
-  if (message.value == "") {
-    message.nextElementSibling.style.display = "block";
-    return false;
+  if (message.value == '') {
+    message.nextElementSibling.style.display = 'block'
+    return false
   } else {
-    message.nextElementSibling.style.display = "none";
+    message.nextElementSibling.style.display = 'none'
   }
 
   //reCaptcha  verifications
-  var response = grecaptcha.getResponse();
+  var response = grecaptcha.getResponse()
   if (response.length == 0) {
-    captchaError.style.display = "block";
-    return false;
+    captchaError.style.display = 'block'
+    return false
   } else {
-    captchaResponseKey = response;
-    captchaError.style.display = "none";
+    captchaResponseKey = response
+    captchaError.style.display = 'none'
   }
 }
 
 function fun() {
-  document.getElementById("name").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("message").value = "";
-  name.nextElementSibling.style.display = "none";
-  email.nextElementSibling.style.display = "none";
-  message.nextElementSibling.style.display = "none";
+  document.getElementById('name').value = ''
+  document.getElementById('email').value = ''
+  document.getElementById('message').value = ''
+  name.nextElementSibling.style.display = 'none'
+  email.nextElementSibling.style.display = 'none'
+  message.nextElementSibling.style.display = 'none'
 }
 
-const thisForm = document.getElementById("customerform");
-thisForm.addEventListener("submit", async function (e) {
-  e.preventDefault();
+const thisForm = document.getElementById('customerform')
+thisForm.addEventListener('submit', async function (e) {
+  e.preventDefault()
   if (validated() === false) {
-    return;
+    return
   }
   const response = await fetch(
-    "https://api-v2.junglecat.com/api/contact/insert",
+    'https://api-v2.junglecat.com/api/contact/insert',
     {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value,
         captchaResponseKey: captchaResponseKey,
       }),
+    },
+  )
+  const result = await response.json()
+  captchaResponseKey = ''
+  modal.hide()
+})
+
+function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this,
+      args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
     }
-  );
-  const result = await response.json();
-  captchaResponseKey = "";
-  modal.hide();
-});
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
+// -------------------------scroll slides-------------------
+
+var slider = document.getElementById('carouselExampleDark')
+var onScroll = debounce(
+  function (direction) {
+    //console.log(direction);
+    if (direction == false) {
+      $('.carousel-control-next').click()
+    } else {
+      $('.carousel-control-prev').click()
+    }
+  },
+  100,
+  true,
+)
+
+slider.addEventListener('wheel', function (e) {
+  e.preventDefault()
+  var delta
+  if (event.wheelDelta) {
+    delta = event.wheelDelta
+  } else {
+    delta = -1 * event.deltaY
+  }
+
+  onScroll(delta >= 0)
+})
